b9c188e86aa6ae873c8f78a87b04a2ce2abf2551
piratte <piratte65@gmail.com>
2017-05-02 12:12:12 +0200

    initial commit

---
+++ HW7/k-means.py
@@ -0,0 +1,14 @@
+
+
+class KMeans:
+    def __init__(self, num_of_clusters):
+        self.k = num_of_clusters
+
+    def fit(self,train_X):
+        pass
+
+    def predict(self, test_X):
+        pass
+
+if __name__ == '__main__':
+    pass
