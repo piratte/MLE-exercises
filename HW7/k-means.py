933c2eb0ef42925c213c03c9643569ee1ad64652
piratte <piratte65@gmail.com>
2017-05-09 21:33:20 +0200

    done K-means + testing on digits dataset

--- HW7/k-means.py
+++ HW7/k-means.py
@@ -1,35 +1,40 @@
 from __future__ import print_function
 
-import random
-
 import numpy as np
 from sklearn.datasets import load_digits
 from sklearn.preprocessing import scale
+from sklearn import metrics
 
 
 class KMeans:
     def __init__(self, num_of_clusters):
         self.k = num_of_clusters
-        self.centroids = []
+        self.centroids = np.array([])
 
     def has_changed(self, new_clusters, clusters):
         return set(new_clusters) != set(clusters)
 
-    def compute_centroids(self, clusters):
-        pass
+    def compute_centroids(self, data, clusters):
+        new_centroids = np.zeros((self.k, 1))
+        for i in range(0, self.k):
+            new_centroids[i] = np.mean(data[clusters==i])
+        return new_centroids
 
     def update_clustering(self, train_X, centroids):
-        pass
+        # vybrat minimalni vzdalenost od bodu k centroidu ~ predict
+        return [self.predict_one(x, centroids) for x in train_X]
 
-    def fit(self, train_X):
+    def fit(self, input_data):
+        train_X = np.array(input_data)
         # random initialization
-        new_clusters = random.sample(train_X, self.k)
-        clusters = random.sample(train_X, self.k)
+        new_clustering = np.random.randint(self.k, size=len(train_X))
+        clustering = np.random.randint(self.k, size=len(train_X))
+        centroids = np.random.randint(0,1,size=(self.k, train_X.shape[1]))
 
-        while self.has_changed(new_clusters, clusters):
-            clusters = new_clusters
-            centroids = self.compute_centroids(clusters)
-            new_clusters = self.update_clustering(train_X, centroids)
+        while self.has_changed(new_clustering, clustering):
+            clustering = new_clustering
+            centroids = self.compute_centroids(train_X, clustering)
+            new_clustering = self.update_clustering(train_X, centroids)
 
         self.centroids = centroids
 
@@ -42,9 +47,18 @@ class KMeans:
 
 if __name__ == '__main__':
     digits = load_digits()
-    data = scale(digits.data)
+    digi_data = scale(digits.data)
 
-    n_samples, n_features = data.shape
+    n_samples, n_features = digi_data.shape
     n_digits = len(np.unique(digits.target))
     labels = digits.target
 
+    perm = np.random.permutation(n_samples)
+    labels, digi_data = labels[perm], digi_data[perm]
+
+    estimator = KMeans(n_digits)
+    estimator.fit(digi_data)
+    estimated_labels = estimator.predict(digi_data)
+    print(metrics.homogeneity_score(labels, estimated_labels),
+          metrics.v_measure_score(labels, estimated_labels),
+          metrics.completeness_score(labels, estimated_labels))
