21f8de93b6d5af973bc9e11e12e363a76c837edd
piratte <piratte65@gmail.com>
2017-05-09 09:10:56 +0200

    k-means work in progress

--- HW7/k-means.py
+++ HW7/k-means.py
@@ -1,14 +1,50 @@
+from __future__ import print_function
+
+import random
+
+import numpy as np
+from sklearn.datasets import load_digits
+from sklearn.preprocessing import scale
 
 
 class KMeans:
     def __init__(self, num_of_clusters):
         self.k = num_of_clusters
+        self.centroids = []
 
-    def fit(self,train_X):
+    def has_changed(self, new_clusters, clusters):
+        return set(new_clusters) != set(clusters)
+
+    def compute_centroids(self, clusters):
         pass
+
+    def update_clustering(self, train_X, centroids):
+        pass
+
+    def fit(self, train_X):
+        # random initialization
+        new_clusters = random.sample(train_X, self.k)
+        clusters = random.sample(train_X, self.k)
+
+        while self.has_changed(new_clusters, clusters):
+            clusters = new_clusters
+            centroids = self.compute_centroids(clusters)
+            new_clusters = self.update_clustering(train_X, centroids)
+
+        self.centroids = centroids
+
+    def predict_one(self, one_X, centroids):
+        dists = [np.linalg.norm(one_X - cent) for cent in centroids]
+        return np.argmin(dists)
 
     def predict(self, test_X):
-        pass
+        return [self.predict_one(x, self.centroids) for x in test_X]
 
 if __name__ == '__main__':
-    pass
+    digits = load_digits()
+    data = scale(digits.data)
+
+    n_samples, n_features = data.shape
+    n_digits = len(np.unique(digits.target))
+    labels = digits.target
+
